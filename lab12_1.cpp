8b536ad1f44347fca6b9a4823e5cdc1a505e9b0f
phurin2644 <phus.2644@gmail.com>
2021-01-19 18:13:12 +0700

    lab12_1com

--- lab12_1.cpp
+++ lab12_1.cpp
@@ -17,3 +17,27 @@ int main(){
     cout << "\nMin = " << B[5];
     return 0;
 }
+void stat(const double A[],int N,double B[]){
+	double ArithmeticMean, StandardDeviation, GeometricMean, HarmonicMean, Max = A[0], Min = A[0];
+	double sum = 0, GM = 1, sum2 = 0, sumh = 0; 
+	for(int i = 0; i < N; i++){
+		sum += A[i];
+		sum2 += pow(A[i],2);
+		GM *= A[i];
+		sumh += 1.0/A[i];	
+	}
+	ArithmeticMean = sum/N;
+	B[0] = ArithmeticMean;
+	StandardDeviation = sqrt((sum2/N)-pow((sum/N),2));
+	B[1] = StandardDeviation;
+	GeometricMean = pow(GM,(1.0/N));
+	B[2] = GeometricMean;
+	HarmonicMean = N/sumh;
+	B[3] = HarmonicMean;
+	for(int i = 1; i < N ; i++){
+		if(A[i] > Max) Max = A[i];
+		if(A[i] < Min) Min = A[i];
+	}
+	B[4] = Max;
+	B[5] = Min;
+}
